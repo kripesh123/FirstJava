13614c12faabe5730e3667cdc9b0b2b5d8012a90
Kripesh Bista <kripeshbista3@gmail.com>
2016-06-20 13:24:15 +0545

    Hello World added

--- FirstJava/src/com/leapfrog/firstjava/Program.java
+++ FirstJava/src/com/leapfrog/firstjava/Program.java
@@ -16,7 +16,7 @@ public class Program {
      */
     public static void main(String[] args) {
         for(int i=1;i<=10;i++){
-            System.out.println(i);
+            System.out.println(i + " HELLO WORLD");
         }
     }
     
